c31c6790473749082d9476b9447dcca6b87b233a
mrWinnee <yassinih490@gmil.com>
2023-03-20 08:01:49 -0700

    msg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int i;
 int j;
 for (i = 0; i < 10; i++)
 {
-  for (j = i+1;j < 10; j++)
-  {
-    putchar('0' + i);
-    putchar('0' + j);
-    putchar(',');
-    putchar(' ');
-  }
+for (j = i + 1; j < 10; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
 return (0);
