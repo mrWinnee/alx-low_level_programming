9869326c7e4ba50437c60f0abfb899fccdbe4d1e
mrWinnee <yassinih490@gmil.com>
2023-03-20 07:51:00 -0700

    msg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,13 +5,12 @@ int main()
   /*
   * c program that write combinations of two digits
   */
-  char nums[] = {'0','1','2','3','4','5','6','7','8','9'};
   int i;
   int j;
   for (i = 0; i < 10; i++) {
     for (j = i+1;j < 10; j++){
-      putchar(nums[i]);
-      putchar(nums[j]);
+      putchar('0' + i);
+      putchar('0' + j);
       putchar(',');
       putchar(' ');
     }
