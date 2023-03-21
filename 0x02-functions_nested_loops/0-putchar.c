8763523a70b227006b854a2eb54c09c21df5b462
mrWinnee <yassinih490@gmil.com>
2023-03-21 04:16:07 -0700

    msg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /*
 * a program that runs
 * and end with 0 if SUCCESS
 */
 int main(void)
 {
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
