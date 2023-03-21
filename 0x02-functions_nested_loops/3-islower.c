db5fd7db5171fdbf33086c2416ef8af1f111a95c
mrWinnee <yassinih490@gmil.com>
2023-03-21 09:56:30 -0700

    msg

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 /*
- * a function that decides if a char s lower or not
+ * _islower - return 1 if the character is lowercase, otherwise 0 .
  */
 int _islower(char c)
 {
-if(c >= 97 && c <= 122)
+if (c >= 97 && c <= 122)
 {
 return (1);
 }
