bc64d32a55162ae833e3fd064e2e37c304792475
mrWinnee <yassinih490@gmil.com>
2023-03-21 09:32:43 -0700

    msg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_alphabet - prints out the alphabet
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+
 void print_alphabet_x10(void)
 {
 int i, j;
