380c89bf8dc9554712cc71b349090404eea32d73
mrWinnee <yassinih490@gmil.com>
2023-03-22 04:33:44 -0700

    msg

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,13 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 if (j > 0 && j < 9)
-printf("%s%d,", (i * j < 10 ? ".." : "."),i * j );
+printf("%s%d,", (i * j < 10 ? "  " : " "), i * j);
 else
 {
 if (j == 0)
-printf("%d,", i *j);
+printf("%d,", i * j);
 else
-printf("%s%d", (i * j < 10 ? ".." : "."),i * j);
+printf("%s%d", (i * j < 10 ? "  " : " "), i * j);
 }
 }
 printf("\n");
