5ddd57b19321f77ddf4917b115c7e1aa5272a693
mrWinnee <yassinih490@gmil.com>
2023-03-23 18:08:15 -0700

    msg

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9.
+ * print_most_numbers - prints the numbers, from 0 to 9, not 2 and 4.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
