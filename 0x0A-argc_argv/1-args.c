c55e28b39eaf3e21239c382ae89dabd96be057de
mrWinnee <yassinih490@gmil.com>
2023-04-13 08:43:58 -0700

    msg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,11 @@
 /**
  * main - checks the code.
  * @argc: size of argv.
+ * @argv: the array.
  * Return: always 0.
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", (argc - 1) + (int) sizeof(argv[0]) * 0);
 	return (0);
 }
