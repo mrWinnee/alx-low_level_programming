764d0768121856ca62c4195aa2356a895775e72e
mrWinnee <yassinih490@gmil.com>
2023-04-13 08:24:33 -0700

    msg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - checks the code.
+ * @argc: the size of the argv[].
+ * @argv: array of the cl arrguments.
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
